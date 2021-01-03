8dbfa85701499b065a4ab38f94d74df585985d40
DVJGamer <diego.villalonju@elorrieta-errekamari.com>
2021-01-03 17:00:26 +0100

    Desarrollando ventana registro

--- EuskWeatherApp/app/src/main/java/com/elorrieta/euskweatherapp/MainActivity.java
+++ EuskWeatherApp/app/src/main/java/com/elorrieta/euskweatherapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.elorrieta.euskweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtUsuario, txtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+    }
+
+    private void RegistroUsuario(View v){
+
     }
 }
